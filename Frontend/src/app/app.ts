052bd5db662616dce2ef94f328c494c49cd5a9b5
Erlind Sejdiu <erlindamprogrammieren@gmail.com>
2026-01-25 15:34:57 +0100

    update

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,16 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PostCard } from './post-card/post-card'
+import { PostCard } from './post-card/post-card';
 import { Header } from './header/header';
+import { Hero } from './hero/hero';
+import { Footer } from './footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-            PostCard,
-            Header
+  imports: [
+    RouterOutlet,
+    PostCard,
+    Header,
+    Hero,
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+  protected readonly title = signal("Erlind's Gedankenwald");
 }
